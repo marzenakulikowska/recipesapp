acd7c8554f86308eb51a4306f0471bb1647aa903
marzenakulikowska <254434@student.pwr.edu.pl>
2024-08-14 21:02:21 +0200

    github pages changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 const Header = ()=>{
     return (
         <div className="logo">
-            <a href="/">RecipeApp</a>
+            <a href="/">RecipesApp</a>
         </div>
     )
 }
